5154cf5f69492f55117331180ccbfc9f8a94cb43
Shota Kishimoto <shokishimo@gmail.com>
2023-02-11 18:23:08 -0600

    add sample test to see if data is inserted

--- controller/home.go
+++ controller/home.go
@@ -1,8 +1,14 @@
 package controller
 
 import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/shokishimo/WhatsTheBestKeyboard/db"
+	"github.com/shokishimo/WhatsTheBestKeyboard/model"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +16,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+
 	ServePublicHome(w)
 }
 
@@ -21,4 +28,36 @@ func ServePublicHome(w http.ResponseWriter) {
 		return
 	}
 	tmpl.Execute(w, nil)
+}
+
+func AddSampleData() {
+	godotenv.Load()
+	database := os.Getenv("DATABASE")
+	keyboardCollection := os.Getenv("COLLECTION_Keyboard")
+	if database == "" || keyboardCollection == "" {
+		fmt.Println("failed to get dtata")
+	}
+	client := db.Connect()
+	// Disconnect from db
+	defer db.Disconnect(client)
+
+	data1 := model.Keyboard{
+		Name:     "Air 75",
+		Make:     "NuPhy",
+		Price:    "$120",
+		Layout:   "75%",
+		Profile:  "low",
+		Switch:   "mechanical",
+		Material: model.Material{},
+		Ranking:  "1",
+	}
+	fmt.Println(data1)
+	// begin insert data
+	collection := client.Database(database).Collection(keyboardCollection)
+	a, err := collection.InsertOne(context.TODO(), data1)
+	fmt.Println(a)
+	if err != nil {
+		fmt.Println("aaa")
+		fmt.Println(err.Error())
+	}
 }
--- model/keyboard.go
+++ model/keyboard.go
@@ -1,14 +1,12 @@
 package model
 
 type Keyboard struct {
-	ID        string   `json:"id"`
-	Name      string   `json:"name"`
-	Make      string   `json:"make"`
-	Price     string   `json:"price"`
-	Layout    int      `json:"layout"`
-	NumOfKeys int      `json:"numOfKeys"`
-	Profile   string   `json:"profile"`
-	Switch    string   `json:"switch"`
-	Material  Material `json:"material"`
-	Ranking   int      `json:"ranking"`
+	Name     string   `json:"name"`
+	Make     string   `json:"make"`
+	Price    string   `json:"price"`
+	Layout   string   `json:"layout"`
+	Profile  string   `json:"profile"`
+	Switch   string   `json:"switch"`
+	Material Material `json:"material"`
+	Ranking  string   `json:"ranking"`
 }
