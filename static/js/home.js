5c8733d7836a77ab3e291087e3bce88d9a54283c
Shota Kishimoto <shokishimo@gmail.com>
2023-02-13 01:48:33 -0600

    dynamically render the ranking on home page

--- static/js/home.js
+++ static/js/home.js
@@ -8,5 +8,28 @@ async function sendGetRequest(query) {
     }
 }
 
-// Example usage
-sendGetRequest("3").then(data => console.log(data));
+sendGetRequest("3").then(data => {
+    let url = data[0].url;
+    let pic = document.getElementById("picture1");
+    pic.style.backgroundImage = `url(${url})`;
+    let rank = data[0].ranking;
+    let rankLetter = document.getElementById("ranking-letter1");
+    rankLetter.textContent = rank
+    rankLetter.setAttribute("style", "font-size: 18px; padding-left: 11px; padding-top: 4px; color: #fff;")
+
+    url = data[1].url;
+    pic = document.getElementById("picture2");
+    pic.style.backgroundImage = `url(${url})`;
+    rank = data[1].ranking;
+    rankLetter = document.getElementById("ranking-letter2");
+    rankLetter.textContent = rank
+    rankLetter.setAttribute("style", "font-size: 18px; padding-left: 11px; padding-top: 4px; color: #fff;")
+
+    url = data[2].url;
+    pic = document.getElementById("picture3");
+    pic.style.backgroundImage = `url(${url})`;
+    rank = data[2].ranking;
+    rankLetter = document.getElementById("ranking-letter3");
+    rankLetter.textContent = rank
+    rankLetter.setAttribute("style", "font-size: 18px; padding-left: 11px; padding-top: 4px; color: #fff;")
+});
