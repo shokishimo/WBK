4b380a3145f7c2780d20bc8d25b42e4431979bdf
Shota Kishimoto <shokishimo@gmail.com>
2023-02-15 01:29:04 -0600

    add login page renderin function

--- controller/login.go
+++ controller/login.go
@@ -1,7 +1,28 @@
 package controller
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		renderLoginPage(w)
+	} else if r.Method == http.MethodPost {
 
+	}
+}
+
+func renderLoginPage(w http.ResponseWriter) {
+	tmpl, err := template.ParseFiles("static/public/login.html")
+	if err != nil {
+		fmt.Println("Error: Failed to get login page")
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error: Failed to render login page")
+		return
+	}
 }
