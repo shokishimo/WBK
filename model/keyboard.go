1ee22da4456f0aa30819936a2da54d9ba674db70
Shota Kishimoto <shokishimo@gmail.com>
2023-02-16 07:43:07 -0600

    add a new variable to keyboard model

--- model/keyboard.go
+++ model/keyboard.go
@@ -9,19 +9,20 @@ import (
 )
 
 type Keyboard struct {
-	Name     string `json:"name"`
-	Make     string `json:"make"`
-	Price    string `json:"price"`
-	Layout   string `json:"layout"`
-	Profile  string `json:"profile"`
-	Switch   string `json:"switch"`
-	Frame    string `json:"frame"`
-	Keycap   string `json:"keycap"`
-	Ranking  string `json:"ranking"`
-	NumVotes string `json:"numvotes"`
-	Score    string `json:"score"`
-	Url      string `json:"url"`
-	Other    string `json:"other"`
+	Name        string `json:"name"`
+	Make        string `json:"make"`
+	Price       string `json:"price"`
+	Layout      string `json:"layout"`
+	Profile     string `json:"profile"`
+	Switch      string `json:"switch"`
+	Frame       string `json:"frame"`
+	Keycap      string `json:"keycap"`
+	Ranking     string `json:"ranking"`
+	NumVotes    string `json:"numvotes"`
+	Score       string `json:"score"`
+	Url         string `json:"url"`
+	OriginalUrl string `json:"originalurl"`
+	Other       string `json:"other"`
 }
 
 func GetRanks(topHowMany int) []Keyboard {
