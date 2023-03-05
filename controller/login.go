4fe7f060b220d390fd8c157ed3eeb7f369b44810
Shota Kishimoto <shokishimo@gmail.com>
2023-03-05 13:58:58 -0600

    edit the login part

--- controller/login.go
+++ controller/login.go
@@ -5,26 +5,18 @@ import (
 	"net/http"
 )
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		renderLoginPage(w)
-	} else if r.Method == http.MethodPost {
-		err := handleLogin(w, r)
-		if err != nil {
-
-		}
-
-	}
+type LoginData struct {
+	ErrorString string
 }
 
-func renderLoginPage(w http.ResponseWriter) {
+func renderLoginPage(w http.ResponseWriter, data LoginData) {
 	tmpl, err := template.ParseFiles("static/public/login.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte("Error: Failed to get login page"))
 		return
 	}
-	err = tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, data)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte("Error: Failed to render login page"))
@@ -32,6 +24,28 @@ func renderLoginPage(w http.ResponseWriter) {
 	}
 }
 
-func handleLogin(w http.ResponseWriter, r *http.Request) error {
-	return nil
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		renderLoginPage(w, LoginData{})
+	} else if r.Method == http.MethodPost {
+		res := handleLogin(w, r)
+		if res != "" {
+			renderLoginPage(w, LoginData{ErrorString: res})
+		}
+
+	}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) string {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	result := ValidateSignupInput(email, password)
+	if result != "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return result
+	}
+
+	// check in database
+
+	return ""
 }
