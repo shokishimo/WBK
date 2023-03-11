6e5c33506bfd0111bf9474e44eb2e05b8ce3f7f5
Shota Kishimoto <shokishimo@gmail.com>
2023-03-10 22:42:09 -0600

    add not found page handle

--- controller/notFound.go
+++ controller/notFound.go
@@ -1,8 +1,17 @@
 package controller
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func Handle404(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
+	tmpl, err := template.ParseFiles("static/public/notFound.html")
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	return
 }
