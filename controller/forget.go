4c1c6ba4b0cc7fdd653eb75a7ea231833f1dc218
Shota Kishimoto <shokishimo@gmail.com>
2023-03-11 23:18:02 -0600

    add forget email page

--- controller/forget.go
+++ controller/forget.go
@@ -1,7 +1,21 @@
 package controller
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func ForgetHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("static/public/forget.html")
+	if err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
 
+	w.Header().Set("Content-Type", "text/html")
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
