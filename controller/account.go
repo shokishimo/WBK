7a7d99a1c34136abb712d8ba135b43af85e1c6f2
Shota Kishimoto <shokishimo@gmail.com>
2023-03-14 23:05:30 -0500

    added account page redering function

--- controller/account.go
+++ controller/account.go
@@ -1,7 +1,25 @@
 package controller
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func AccountHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	tmpl, err := template.ParseFiles("static/public/account.html")
+	if err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
 
+	w.Header().Set("Content-Type", "text/html")
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
